7bbc379119ad47c954cdb637a18e0a8d3e758233
Mason <122707566+MLA-Format@users.noreply.github.com>
2024-09-08 00:48:08 -0400

    Changed coordinate values to user input

--- fracturing.c
+++ fracturing.c
@@ -19,11 +19,11 @@ double calculateDiameter(int x1, int y1, int x2, int y2) {
 double calculateDistance() {
 
     int x1 = askForUserInput();
-    int y1 = 2;
+    int y1 = askForUserInput();
     printf("Point #1 entered: x1 = %i; y1= %i\n", x1, y1);
 
-    int x2 = 3;
-    int y2 = 2;
+    int x2 = askForUserInput();
+    int y2 = askForUserInput();
     printf("Point #2 entered: x1 = %i; y1= %i\n", x2, y2);
 
     float diameter = calculateDiameter(x1, y1, x2, y2);
@@ -36,12 +36,12 @@ double calculateDistance() {
 
 double calculatePerimiter() {
 
-    int x1 = 2;
-    int y1 = 2;
+    int x1 = askForUserInput();
+    int y1 = askForUserInput();
     printf("Point #1 entered: x1 = %i; y1= %i\n", x1, y1);
 
-    int x2 = 3;
-    int y2 = 2;
+    int x2 = askForUserInput();
+    int y2 = askForUserInput();
     printf("Point #2 entered: x1 = %i; y1= %i\n", x2, y2);
 
     float perimeter = calculateDiameter(x1, y1, x2, y2) * PI;
@@ -54,12 +54,12 @@ double calculatePerimiter() {
 
 double calculateArea() {
 
-    int x1 = 2;
-    int y1 = 2;
+    int x1 = askForUserInput();
+    int y1 = askForUserInput();
     printf("Point #1 entered: x1 = %i; y1= %i\n", x1, y1);
 
-    int x2 = 3;
-    int y2 = 2;
+    int x2 = askForUserInput();
+    int y2 = askForUserInput();
     printf("Point #2 entered: x1 = %i; y1= %i\n", x2, y2);
 
     float area = pow(calculateDiameter(x1, y1, x2, y2)/2, 2) * PI;
@@ -70,12 +70,12 @@ double calculateArea() {
 }
 
 double calculateWidth() {
-    int x1 = 2;
-    int y1 = 2;
+    int x1 = askForUserInput();
+    int y1 = askForUserInput();
     printf("Point #1 entered: x1 = %i; y1= %i\n", x1, y1);
 
-    int x2 = 3;
-    int y2 = 2;
+    int x2 = askForUserInput();
+    int y2 = askForUserInput();
     printf("Point #2 entered: x1 = %i; y1= %i\n", x2, y2);
 
     double width = calculateDiameter(x1, y1, x2, y2);
@@ -86,12 +86,12 @@ double calculateWidth() {
 }
 
 double calculateHeight() {
-    int x1 = 2;
-    int y1 = 2;
+    int x1 = askForUserInput();
+    int y1 = askForUserInput();
     printf("Point #1 entered: x1 = %i; y1= %i\n", x1, y1);
 
-    int x2 = 3;
-    int y2 = 2;
+    int x2 = askForUserInput();
+    int y2 = askForUserInput();
     printf("Point #2 entered: x1 = %i; y1= %i\n", x2, y2);
 
     double height = calculateDiameter(x1, y1, x2, y2);
@@ -103,6 +103,10 @@ double calculateHeight() {
 
 int main(int argc, char **argvcc) {
     calculateDistance();
+    calculatePerimiter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
 
     return 0;
 } // End of main.
